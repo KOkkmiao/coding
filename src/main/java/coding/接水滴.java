b653ff81311d852e950c318362000024fc719c9b
miaoxiaopeng <2388873680@qq.com>
2024-11-12 10:01:25 +0800

    接水滴

---
+++ src/main/java/coding/接水滴.java
@@ -0,0 +1,24 @@
+package coding;
+
+/**
+ * @Author: author
+ * @Description:
+ * @Date: 2024/11/12 9:57 上午
+ * @Version: 1.0
+ */
+public class 接水滴 {
+    public int maxArea(int[] height) {
+        int left =0;
+        int right =height.length-1;
+        int res = 0;
+        for (int i = 0; i < height.length; i++) {
+            int heightv = Math.max(height[left],height[right]);
+            res = Math.max((right-left)*heightv,res);
+            if (height[left]<height[right]){
+                left++;
+            }else {
+                right++;
+            }
+        }
+    }
+}
