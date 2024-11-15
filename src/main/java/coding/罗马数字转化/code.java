becdd8ad30ba0a5ab54291f67030ee5495c38f09
miaoxiaopeng <2388873680@qq.com>
2024-11-15 10:20:14 +0800

    补充第二种计算方法

--- src/main/java/coding/罗马数字转化/code.java
+++ src/main/java/coding/罗马数字转化/code.java
@@ -24,8 +24,22 @@ public class code {
      * 如果该值以 4 或 9 开头，使用 减法形式，表示从以下符号中减去一个符号，例如 4 是 5 (V) 减 1 (I): IV ，9 是 10 (X) 减 1 (I)：IX。仅使用以下减法形式：4 (IV)，9 (IX)，40 (XL)，90 (XC)，400 (CD) 和 900 (CM)。
      * 只有 10 的次方（I, X, C, M）最多可以连续附加 3 次以代表 10 的倍数。你不能多次附加 5 (V)，50 (L) 或 500 (D)。如果需要将符号附加4次，请使用 减法形式。
      * 给定一个整数，将其转换为罗马数字。
-     *
+     * **1 <= num <= 3999**
      */
+    public String convertNumberSecond(int num){
+        // 如果知道数字限制的长度 可以预先算出来有多少种可能。
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        StringBuffer roman = new StringBuffer();
+        roman.append(thousands[num / 1000]);
+        roman.append(hundreds[num % 1000 / 100]);
+        roman.append(tens[num % 100 / 10]);
+        roman.append(ones[num % 10]);
+        return roman.toString();
+
+    }
     public String convertNumber(int num){
         int[] value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] mapValue = {
